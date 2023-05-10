f8576b6ca022c3973fd60d3adf43fcd1b6cc3586
toffycaluga <95626402+toffycaluga@users.noreply.github.com>
2023-05-10 14:55:10 -0300

    desafio notas finales

---
+++ desafios/modulo_2/javascript/notas_finales/assets/js/index.js
@@ -0,0 +1,29 @@
+
+const notas = (ramo) => {
+    const notas = [];
+    notas.push(prompt(`ingrese nota 1 [${ramo}]`));
+    notas.push(prompt(`ingrese nota 2 [${ramo}]`));
+    notas.push(prompt(`ingrese nota 3 [${ramo}]`));
+    console.log(notas);
+    const promedio = calcularPromedio(notas);
+    instertarHtml(ramo, notas, promedio)
+}
+
+const instertarHtml = (ramo, notas, promedio) => {
+    const newRamo = document.getElementById(ramo)
+    console.log(ramo);
+    newRamo.innerHTML = `
+                    <th>${ramo.toUpperCase()}</th>
+                    <th >${notas[0]}</th>
+                    <th >${notas[1]}</th>
+                    <th >${notas[2]}</th>
+                    <th >${promedio}</th>
+                    <td><button class="btn btn-primary" onclick="notas('${ramo}');">agregar notas</button></td>
+    `
+}
+
+const calcularPromedio = (notas) => {
+    let acum = 0
+    notas.forEach(nota => acum += parseInt(nota))
+    return acum / notas.length
+}
